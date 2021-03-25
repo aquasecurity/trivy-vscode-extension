95270a7eb9f346454e4f1e36fc15a495ac92d02a
Simarpreet Singh <simar@linux.com>
2021-03-24 19:18:39 -0700

    feat: Fix Trivy global --quiet option
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -9,52 +9,55 @@ suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 	
 	test('Not a vulnerable project', () => {
-		var got = trivyExtension.runCommand("trivy filesystem --quiet --exit-code=10", "src/test/golden/not-vulnerable");
-		assert.strictEqual(got, ``, "should be equal and no vulnerablities found");
+		var got = trivyExtension.runCommand("trivy --quiet filesystem --exit-code=10", "src/test/golden/not-vulnerable");
+		assert.strictEqual(got, ``, "should be equal and no vulnerabilities found");
 	});
 
 	test('Vulnerable project', () => {
-		var got = trivyExtension.runCommand("trivy filesystem --quiet --exit-code=10", "src/test/golden/vulnerable");
+		var got = trivyExtension.runCommand("trivy --quiet filesystem --exit-code=10", "src/test/golden/vulnerable");
 		assert.strictEqual(got, `
 package-lock.json
 =================
-Total: 9 (UNKNOWN: 0, LOW: 1, MEDIUM: 5, HIGH: 3, CRITICAL: 0)
+Total: 8 (UNKNOWN: 0, LOW: 1, MEDIUM: 4, HIGH: 2, CRITICAL: 1)
 
-+---------+------------------+----------+-------------------+---------------+--------------------------------+
-| LIBRARY | VULNERABILITY ID | SEVERITY | INSTALLED VERSION | FIXED VERSION |             TITLE              |
-+---------+------------------+----------+-------------------+---------------+--------------------------------+
-| jquery  | CVE-2019-11358   | MEDIUM   | 3.3.9             | 3.4.0         | js-jquery: prototype pollution |
-|         |                  |          |                   |               | in object's prototype leading  |
-|         |                  |          |                   |               | to denial of service or...     |
-+         +------------------+          +                   +               +--------------------------------+
-|         | CVE-2019-5428    |          |                   |               | Modification of                |
-|         |                  |          |                   |               | Assumed-Immutable Data (MAID)  |
-+         +------------------+          +                   +---------------+--------------------------------+
-|         | CVE-2020-11022   |          |                   | 3.5.0         | jquery: Cross-site             |
-|         |                  |          |                   |               | scripting due to improper      |
-|         |                  |          |                   |               | injQuery.htmlPrefilter method  |
-+         +------------------+          +                   +               +--------------------------------+
-|         | CVE-2020-11023   |          |                   |               | Potential XSS vulnerability in |
-|         |                  |          |                   |               | jQuery                         |
-+---------+------------------+----------+-------------------+---------------+--------------------------------+
-| lodash  | CVE-2018-16487   | HIGH     | 4.17.4            | 4.17.11       | lodash: Prototype pollution in |
-|         |                  |          |                   |               | utilities function             |
-+         +------------------+          +                   +---------------+--------------------------------+
-|         | CVE-2019-10744   |          |                   | 4.17.12       | nodejs-lodash: prototype       |
-|         |                  |          |                   |               | pollution in defaultsDeep      |
-|         |                  |          |                   |               | function leading to modifying  |
-|         |                  |          |                   |               | properties                     |
-+         +------------------+          +                   +---------------+--------------------------------+
-|         | NSWG-ECO-516     |          |                   |               | Allocation of Resources        |
-|         |                  |          |                   |               | Without Limits or Throttling   |
-+         +------------------+----------+                   +---------------+--------------------------------+
-|         | CVE-2019-1010266 | MEDIUM   |                   | 4.17.11       | Moderate severity              |
-|         |                  |          |                   |               | vulnerability that affects     |
-|         |                  |          |                   |               | lodash                         |
-+         +------------------+----------+                   +---------------+--------------------------------+
-|         | CVE-2018-3721    | LOW      |                   | 4.17.5        | lodash: Prototype pollution in |
-|         |                  |          |                   |               | utilities function             |
-+---------+------------------+----------+-------------------+---------------+--------------------------------+
++---------+------------------+----------+-------------------+---------------+-----------------------------------------+
+| LIBRARY | VULNERABILITY ID | SEVERITY | INSTALLED VERSION | FIXED VERSION |                  TITLE                  |
++---------+------------------+----------+-------------------+---------------+-----------------------------------------+
+| jquery  | CVE-2019-11358   | MEDIUM   | 3.3.9             | 3.4.0         | jquery: Prototype pollution in          |
+|         |                  |          |                   |               | object's prototype leading to           |
+|         |                  |          |                   |               | denial of service, remote...            |
+|         |                  |          |                   |               | -->avd.aquasec.com/nvd/cve-2019-11358   |
++         +------------------+          +                   +---------------+-----------------------------------------+
+|         | CVE-2020-11022   |          |                   | 3.5.0         | jquery: Cross-site                      |
+|         |                  |          |                   |               | scripting due to improper               |
+|         |                  |          |                   |               | injQuery.htmlPrefilter method           |
+|         |                  |          |                   |               | -->avd.aquasec.com/nvd/cve-2020-11022   |
++         +------------------+          +                   +               +-----------------------------------------+
+|         | CVE-2020-11023   |          |                   |               | jquery: Passing HTML containing         |
+|         |                  |          |                   |               | <option> elements to manipulation       |
+|         |                  |          |                   |               | methods could result in...              |
+|         |                  |          |                   |               | -->avd.aquasec.com/nvd/cve-2020-11023   |
++---------+------------------+----------+-------------------+---------------+-----------------------------------------+
+| lodash  | CVE-2019-10744   | CRITICAL | 4.17.4            | 4.17.12       | nodejs-lodash: prototype                |
+|         |                  |          |                   |               | pollution in defaultsDeep function      |
+|         |                  |          |                   |               | leading to modifying properties         |
+|         |                  |          |                   |               | -->avd.aquasec.com/nvd/cve-2019-10744   |
++         +------------------+----------+                   +---------------+-----------------------------------------+
+|         | CVE-2018-16487   | HIGH     |                   | 4.17.11       | lodash: Prototype pollution             |
+|         |                  |          |                   |               | in utilities function                   |
+|         |                  |          |                   |               | -->avd.aquasec.com/nvd/cve-2018-16487   |
++         +------------------+          +                   +---------------+-----------------------------------------+
+|         | CVE-2020-8203    |          |                   | 4.17.19       | nodejs-lodash: prototype pollution      |
+|         |                  |          |                   |               | in zipObjectDeep function               |
+|         |                  |          |                   |               | -->avd.aquasec.com/nvd/cve-2020-8203    |
++         +------------------+----------+                   +---------------+-----------------------------------------+
+|         | CVE-2019-1010266 | MEDIUM   |                   | 4.17.11       | Prototype pollution in lodash           |
+|         |                  |          |                   |               | -->avd.aquasec.com/nvd/cve-2019-1010266 |
++         +------------------+----------+                   +---------------+-----------------------------------------+
+|         | CVE-2018-3721    | LOW      |                   | 4.17.5        | lodash: Prototype pollution             |
+|         |                  |          |                   |               | in utilities function                   |
+|         |                  |          |                   |               | -->avd.aquasec.com/nvd/cve-2018-3721    |
++---------+------------------+----------+-------------------+---------------+-----------------------------------------+
 `, "should be equal and vulnerablities found");
 	});
 });
