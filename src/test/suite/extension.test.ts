da5f24e07f5073a52f845564c2f81944a83041e0
Owen Rumney <owen.rumney@aquasec.com>
2025-02-03 17:42:31 +0000

    chore: Update dependencies and tests (#56)
    chore: Update dependencies
    
    - Update dependencies
    - Fix tests with breaking Mocha change
    - Add npm script to run tests
    - Add tests to the github workflows
    - fix formatting in the extension test
    
    ---------
    
    Co-authored-by: Nikita Pivkin <nikita.pivkin@smartforce.io>

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,63 +1,29 @@
 import * as assert from 'assert';
+import * as fs from 'fs';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
 import * as trivyExtension from '../../extension';
+import path from 'path';
 
-suite('Extension Test Suite', () => {
+	const testsRoot = path.resolve(__dirname, '..');
+
+suite('Extension Test Suite', function (): void {
+	// set the timeout to give test-electron time 
+	// to download vscode-test and unzip it in CI
+	this.timeout(10000);
 	vscode.window.showInformationMessage('Start all tests.');
-	
+
 	test('Not a vulnerable project', () => {
-		var got = trivyExtension.runCommand("trivy --quiet filesystem --exit-code=10", "src/test/golden/not-vulnerable");
-		assert.strictEqual(got, ``, "should be equal and no vulnerabilities found");
+		const got = trivyExtension.runCommand("trivy --quiet filesystem --exit-code=10", testsRoot + "/golden/not-vulnerable");
+		const expected  = '';
+		assert.strictEqual(got, expected, "should be equal and no vulnerabilities found");
 	});
 
 	test('Vulnerable project', () => {
-		var got = trivyExtension.runCommand("trivy --quiet filesystem --exit-code=10", "src/test/golden/vulnerable");
-		assert.strictEqual(got, `
-package-lock.json
-=================
-Total: 8 (UNKNOWN: 0, LOW: 1, MEDIUM: 4, HIGH: 2, CRITICAL: 1)
-
-+---------+------------------+----------+-------------------+---------------+-----------------------------------------+
-| LIBRARY | VULNERABILITY ID | SEVERITY | INSTALLED VERSION | FIXED VERSION |                  TITLE                  |
-+---------+------------------+----------+-------------------+---------------+-----------------------------------------+
-| jquery  | CVE-2019-11358   | MEDIUM   | 3.3.9             | 3.4.0         | jquery: Prototype pollution in          |
-|         |                  |          |                   |               | object's prototype leading to           |
-|         |                  |          |                   |               | denial of service, remote...            |
-|         |                  |          |                   |               | -->avd.aquasec.com/nvd/cve-2019-11358   |
-+         +------------------+          +                   +---------------+-----------------------------------------+
-|         | CVE-2020-11022   |          |                   | 3.5.0         | jquery: Cross-site                      |
-|         |                  |          |                   |               | scripting due to improper               |
-|         |                  |          |                   |               | injQuery.htmlPrefilter method           |
-|         |                  |          |                   |               | -->avd.aquasec.com/nvd/cve-2020-11022   |
-+         +------------------+          +                   +               +-----------------------------------------+
-|         | CVE-2020-11023   |          |                   |               | jquery: Passing HTML containing         |
-|         |                  |          |                   |               | <option> elements to manipulation       |
-|         |                  |          |                   |               | methods could result in...              |
-|         |                  |          |                   |               | -->avd.aquasec.com/nvd/cve-2020-11023   |
-+---------+------------------+----------+-------------------+---------------+-----------------------------------------+
-| lodash  | CVE-2019-10744   | CRITICAL | 4.17.4            | 4.17.12       | nodejs-lodash: prototype                |
-|         |                  |          |                   |               | pollution in defaultsDeep function      |
-|         |                  |          |                   |               | leading to modifying properties         |
-|         |                  |          |                   |               | -->avd.aquasec.com/nvd/cve-2019-10744   |
-+         +------------------+----------+                   +---------------+-----------------------------------------+
-|         | CVE-2018-16487   | HIGH     |                   | 4.17.11       | lodash: Prototype pollution             |
-|         |                  |          |                   |               | in utilities function                   |
-|         |                  |          |                   |               | -->avd.aquasec.com/nvd/cve-2018-16487   |
-+         +------------------+          +                   +---------------+-----------------------------------------+
-|         | CVE-2020-8203    |          |                   | 4.17.19       | nodejs-lodash: prototype pollution      |
-|         |                  |          |                   |               | in zipObjectDeep function               |
-|         |                  |          |                   |               | -->avd.aquasec.com/nvd/cve-2020-8203    |
-+         +------------------+----------+                   +---------------+-----------------------------------------+
-|         | CVE-2019-1010266 | MEDIUM   |                   | 4.17.11       | Prototype pollution in lodash           |
-|         |                  |          |                   |               | -->avd.aquasec.com/nvd/cve-2019-1010266 |
-+         +------------------+----------+                   +---------------+-----------------------------------------+
-|         | CVE-2018-3721    | LOW      |                   | 4.17.5        | lodash: Prototype pollution             |
-|         |                  |          |                   |               | in utilities function                   |
-|         |                  |          |                   |               | -->avd.aquasec.com/nvd/cve-2018-3721    |
-+---------+------------------+----------+-------------------+---------------+-----------------------------------------+
-`, "should be equal and vulnerablities found");
+		const got = trivyExtension.runCommand("trivy --quiet filesystem --exit-code=10", testsRoot + "/golden/vulnerable");
+		const expected = fs.readFileSync(testsRoot + "/golden/expected/vulnerable.out", "utf8");
+		assert.strictEqual(got, expected, "should be equal and vulnerablities found");
 	});
 });
