0dd7bfb0a722935382b1a69c35fdfe0ce6366161
Owen Rumney <owen.rumney@aquasec.com>
2025-03-27 15:31:03 +0000

    feat: update the tree layout for aqua platform
    When the user has aqua platform enabled, the layout of the treeview has
    a slightly different view.
    
    Category
      - Severity
        - Filepath
          - Issue

--- src/explorer/treeview/findings.ts
+++ src/explorer/treeview/findings.ts
@@ -3,6 +3,7 @@ import * as vscode from 'vscode';
 import {
   Misconfiguration,
   PolicyResult,
+  Secret,
   TrivyResult,
   Vulnerability,
 } from '../result';
@@ -11,12 +12,82 @@ import { TrivyTreeItem } from './treeitem';
 import { TrivyTreeItemType } from './treeitem_types';
 import { createFileOpenCommand } from './treeview_command';
 
+const SEVERITY_ORDER = ['Critical', 'High', 'Medium', 'Low', 'Unknown'];
+
 /**
  * Extract the top level findings from the result data
  * @param resultData to extract the top level findings from
  * @returns The top level findings as TrivyTreeItems
  */
 export function getTopLevelFindings<T extends TrivyResult | PolicyResult>(
+  resultData: T[]
+): TrivyTreeItem[] {
+  const config = vscode.workspace.getConfiguration('trivy');
+  const useAquaPlatform = config.get<boolean>('useAquaPlatform');
+  if (useAquaPlatform) {
+    return groupResultsBySeverity(resultData);
+  } else {
+    return groupResultsByFile(resultData);
+  }
+}
+
+function groupResultsBySeverity<T extends TrivyResult | PolicyResult>(
+  resultData: T[]
+): TrivyTreeItem[] {
+  const results: TrivyTreeItem[] = [];
+
+  if (
+    resultData.filter(
+      (c) => c instanceof TrivyResult && c.extraData instanceof Vulnerability
+    ).length > 0
+  ) {
+    results.push(
+      new TrivyTreeItem(
+        resultData[0].workspaceName,
+        'Vulnerabilities',
+        vscode.TreeItemCollapsibleState.Expanded,
+        TrivyTreeItemType.vulnerabilityRoot,
+        {}
+      )
+    );
+  }
+
+  if (
+    resultData.filter(
+      (c) => c instanceof TrivyResult && c.extraData instanceof Misconfiguration
+    ).length > 0
+  ) {
+    results.push(
+      new TrivyTreeItem(
+        resultData[0].workspaceName,
+        'Misconfigurations',
+        vscode.TreeItemCollapsibleState.Expanded,
+        TrivyTreeItemType.misconfigRoot,
+        {}
+      )
+    );
+  }
+
+  if (
+    resultData.filter(
+      (c) => c instanceof TrivyResult && c.extraData instanceof Secret
+    ).length > 0
+  ) {
+    results.push(
+      new TrivyTreeItem(
+        resultData[0].workspaceName,
+        'Sensitive Data',
+        vscode.TreeItemCollapsibleState.Expanded,
+        TrivyTreeItemType.secretRoot,
+        {}
+      )
+    );
+  }
+
+  return results;
+}
+
+function groupResultsByFile<T extends TrivyResult | PolicyResult>(
   resultData: T[]
 ): TrivyTreeItem[] {
   const results: TrivyTreeItem[] = [];
@@ -112,7 +183,10 @@ export function getVulnerabilityChildren<T extends TrivyResult | PolicyResult>(
       c instanceof TrivyResult &&
       c.extraData instanceof Vulnerability &&
       c.extraData.pkgName === element.title &&
-      c.filename === element.filename
+      c.filename === element.filename &&
+      (element.properties?.requiredSeverity === undefined ||
+        c.severity.toLowerCase() ===
+          element.properties?.requiredSeverity.toLowerCase())
   );
 
   for (let index = 0; index < filtered.length; index++) {
@@ -130,7 +204,11 @@ export function getVulnerabilityChildren<T extends TrivyResult | PolicyResult>(
       title,
       collapsedState,
       TrivyTreeItemType.vulnerabilityCode,
-      { check: result, command: createFileOpenCommand(result) }
+      {
+        check: result,
+        command: createFileOpenCommand(result),
+        requiredSeverity: result.severity,
+      }
     );
     results.push(item);
   }
@@ -173,6 +251,212 @@ export function getSecretInstances<T extends TrivyResult | PolicyResult>(
       { check: result, command: createFileOpenCommand(result) }
     );
     results.push(item);
+  }
+
+  return results;
+}
+
+export function getVulnerabilitySeverityRoots<
+  T extends TrivyResult | PolicyResult,
+>(resultData: T[]): TrivyTreeItem[] {
+  const results: TrivyTreeItem[] = [];
+
+  const filtered = resultData.filter(
+    (c: PolicyResult | TrivyResult) =>
+      c instanceof TrivyResult && c.extraData instanceof Vulnerability
+  );
+
+  const severities = Array.from(new Set(filtered.map((c) => c.severity)));
+
+  SEVERITY_ORDER.forEach((sev) => {
+    if (severities.includes(sev.toUpperCase())) {
+      results.push(
+        new TrivyTreeItem(
+          resultData[0].workspaceName,
+          sev,
+          vscode.TreeItemCollapsibleState.Collapsed,
+          TrivyTreeItemType.vulnerabilitySeverity,
+          { requiredSeverity: sev }
+        )
+      );
+    }
+  });
+
+  return results;
+}
+
+export function getMisconfigSeverityRoots(
+  resultData: (TrivyResult | PolicyResult)[]
+): TrivyTreeItem[] {
+  const results: TrivyTreeItem[] = [];
+
+  const filtered = resultData.filter(
+    (c: PolicyResult | TrivyResult) =>
+      c instanceof TrivyResult && c.extraData instanceof Misconfiguration
+  );
+
+  const severities = Array.from(new Set(filtered.map((c) => c.severity)));
+
+  SEVERITY_ORDER.forEach((sev) => {
+    if (severities.includes(sev.toUpperCase())) {
+      results.push(
+        new TrivyTreeItem(
+          resultData[0].workspaceName,
+          sev,
+          vscode.TreeItemCollapsibleState.Collapsed,
+          TrivyTreeItemType.misconfigSeverity,
+          {
+            requiredSeverity: sev,
+          }
+        )
+      );
+    }
+  });
+  return results;
+}
+
+export function getSecretRoots(
+  resultData: (TrivyResult | PolicyResult)[]
+): TrivyTreeItem[] {
+  const results: TrivyTreeItem[] = [];
+
+  const filtered = resultData.filter(
+    (c: PolicyResult | TrivyResult) =>
+      c instanceof TrivyResult && c.extraData instanceof Secret
+  );
+
+  const severities = Array.from(new Set(filtered.map((c) => c.severity)));
+
+  SEVERITY_ORDER.forEach((sev) => {
+    if (severities.includes(sev.toUpperCase())) {
+      results.push(
+        new TrivyTreeItem(
+          resultData[0].workspaceName,
+          sev,
+          vscode.TreeItemCollapsibleState.Collapsed,
+          TrivyTreeItemType.secretSeverity,
+          { requiredSeverity: sev }
+        )
+      );
+    }
+  });
+
+  return results;
+}
+
+export function getVulnerabilitiesBySeverity<
+  T extends TrivyResult | PolicyResult,
+>(resultData: T[], severity: string): TrivyTreeItem[] {
+  const results: TrivyTreeItem[] = [];
+  const resolvedNodes: string[] = [];
+
+  const filtered = resultData.filter(
+    (c: PolicyResult | TrivyResult) =>
+      c instanceof TrivyResult &&
+      c.extraData instanceof Vulnerability &&
+      c.severity.toLowerCase() === severity.toLowerCase()
+  );
+
+  for (let index = 0; index < filtered.length; index++) {
+    const result = filtered[index];
+    if (result === undefined) {
+      continue;
+    }
+
+    if (resolvedNodes.includes(result.filename)) {
+      continue;
+    }
+
+    resolvedNodes.push(result.filename);
+
+    results.push(
+      new TrivyTreeItem(
+        result.workspaceName,
+        result.filename,
+        vscode.TreeItemCollapsibleState.Collapsed,
+        TrivyTreeItemType.vulnerabilityFile,
+        { check: result, requiredSeverity: severity }
+      )
+    );
+  }
+
+  return results;
+}
+
+export function getMisconfigurationsBySeverity<
+  T extends TrivyResult | PolicyResult,
+>(resultData: T[], severity: string): TrivyTreeItem[] {
+  const results: TrivyTreeItem[] = [];
+  const resolvedNodes: string[] = [];
+
+  const filtered = resultData.filter(
+    (c: PolicyResult | TrivyResult) =>
+      c instanceof TrivyResult &&
+      c.extraData instanceof Misconfiguration &&
+      c.severity.toLowerCase() === severity.toLowerCase()
+  );
+
+  for (let index = 0; index < filtered.length; index++) {
+    const result = filtered[index];
+    if (result === undefined) {
+      continue;
+    }
+
+    if (resolvedNodes.includes(result.filename)) {
+      continue;
+    }
+
+    resolvedNodes.push(result.filename);
+
+    results.push(
+      new TrivyTreeItem(
+        result.workspaceName,
+        result.filename,
+        vscode.TreeItemCollapsibleState.Collapsed,
+        TrivyTreeItemType.misconfigFile,
+        { check: result, requiredSeverity: severity }
+      )
+    );
+  }
+
+  return results;
+}
+
+export function getSecretsBySeverity<T extends TrivyResult | PolicyResult>(
+  resultData: T[],
+  severity: string
+): TrivyTreeItem[] {
+  const results: TrivyTreeItem[] = [];
+  const resolvedNodes: string[] = [];
+
+  const filtered = resultData.filter(
+    (c: PolicyResult | TrivyResult) =>
+      c instanceof TrivyResult &&
+      c.extraData instanceof Secret &&
+      c.severity.toLowerCase() === severity.toLowerCase()
+  );
+
+  for (let index = 0; index < filtered.length; index++) {
+    const result = filtered[index];
+    if (result === undefined) {
+      continue;
+    }
+
+    if (resolvedNodes.includes(result.filename)) {
+      continue;
+    }
+
+    resolvedNodes.push(result.filename);
+
+    results.push(
+      new TrivyTreeItem(
+        result.workspaceName,
+        result.filename,
+        vscode.TreeItemCollapsibleState.Collapsed,
+        TrivyTreeItemType.secretFile,
+        { check: result, requiredSeverity: severity }
+      )
+    );
   }
 
   return results;
